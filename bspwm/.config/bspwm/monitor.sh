4a949b8769d6a05b3bbfdcf32b33b40b6bf1c2ab
sudoLife <mt.ovinov@gmail.com>
2018-12-31 11:40:27 +0200

    Replace monitors' names with variables

--- bspwm/.config/bspwm/monitor.sh
+++ bspwm/.config/bspwm/monitor.sh
@@ -12,23 +12,23 @@ then
 	
 	if [ "$hdmi" = "connected" ]
 	then
-		xrandr --output HDMI-1 --mode 1920x1080
-		bspc monitor HDMI-1 -d I II III IV V VI
-		bspc monitor --focus HDMI-1
+		xrandr --output $hdmi --mode 1920x1080
+		bspc monitor $hdmi -d I II III IV V VI
+		bspc monitor --focus $hdmi
 	fi
 
 	if [ "$vga" == "connected" ]
 	then
-		bspc monitor VGA-1 -d I II III IV V VI
-		bspc monitor --focus VGA-1
+		bspc monitor $vga -d I II III IV V VI
+		bspc monitor --focus $vga
 	fi
 
 	if [[ "$hdmi" = "connected" && "$vga" = "connected" ]]
 	then
-		xrandr --output HDMI-1 --right-of VGA-1
-		bspc monitor VGA-1 -d I II III
-		bspc monitor HDMI-1 -d IV V VI 
-		bspc monitor --focus HDMI-1
+		xrandr --output $hdmi --right-of $vga
+		bspc monitor $vga -d I II III
+		bspc monitor $hdmi -d IV V VI 
+		bspc monitor --focus $hdmi
 	fi
 fi	
 
